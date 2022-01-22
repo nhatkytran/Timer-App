e824ae333fb86ef37e3997c3abeb3b0b7f42c724
nhatkytran <nhatky.tran.2002@gmail.com>
2022-01-22 15:22:06 +0700

    Timer App

--- script.js
+++ script.js
@@ -3,5 +3,44 @@ import Timer from './timer.js';
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const resetButton = document.querySelector('#reset');
+const circle = document.querySelector('circle');
 
-new Timer(durationInput, startButton, resetButton);
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+const circleStart = function () {
+  circle.setAttribute('stroke-dasharray', perimeter);
+  circle.setAttribute('stroke-dashoffset', 0);
+};
+circleStart();
+
+let velocity = 0;
+new Timer(durationInput, startButton, resetButton, {
+  onStart(duration) {
+    startButton.innerHTML = '<i class="fas fa-pause"></i>';
+    durationInput.style.animation = `colorChange linear ${duration}s`;
+    durationInput.style.webkitAnimationPlayState = 'running';
+  },
+  onPause() {
+    startButton.innerHTML = '<i class="fas fa-play"></i>';
+    durationInput.style.color = 'red';
+    durationInput.style.webkitAnimationPlayState = 'paused';
+  },
+  onTick(duration) {
+    velocity -= (perimeter * 0.02) / duration;
+    circle.setAttribute('stroke-dashoffset', velocity);
+  },
+  onChange() {
+    this.onReset();
+  },
+  onReset() {
+    circleStart();
+    velocity = 0;
+    durationInput.style.color = 'orange';
+    durationInput.style.animation = `none`;
+    circle.classList.remove('completed');
+  },
+  onComplete() {
+    circleStart();
+    durationInput.style.color = 'red';
+    circle.classList.add('completed');
+  },
+});
--- timer.js
+++ timer.js
@@ -1,12 +1,40 @@
 'use strict';
 
 class Timer {
-  constructor(durationInput, startButton, resetButton) {
+  constructor(durationInput, startButton, resetButton, callbacks) {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.resetButton = resetButton;
+    this.startingValue = this.durationInput.value;
+    this.nuance = 'noPlaying';
+    this.isChange = false;
 
-    this.startButton.addEventListener('click', this.start.bind(this));
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onPause = callbacks.onPause;
+      this.onTick = callbacks.onTick;
+      this.onChange = callbacks.onChange;
+      this.onReset = callbacks.onReset;
+      this.onComplete = callbacks.onComplete;
+    }
+
+    this.durationInput.addEventListener('focus', () => {
+      this.change();
+    });
+
+    this.startButton.addEventListener('click', () => {
+      if (this.nuance === 'noPlaying' && this.timeRemaining > 0) {
+        this.start();
+      } else if (this.nuance === 'playing') {
+        this.pause();
+      }
+    });
+
+    this.resetButton.addEventListener('click', () => {
+      this.durationInput.value = this.startingValue;
+      this.pause();
+      this.onReset();
+    });
   }
 
   get timeRemaining() {
@@ -18,16 +46,36 @@ class Timer {
   }
 
   start() {
+    this.nuance = 'playing';
+    if (this.onStart) this.onStart(this.timeRemaining);
     this.interval = setInterval(this.tick.bind(this), 20);
+
+    if (this.isChange) {
+      this.startingValue = this.durationInput.value;
+      this.isChange = false;
+    }
   }
 
   pause() {
+    this.nuance = 'noPlaying';
+    if (this.onPause) this.onPause();
     clearInterval(this.interval);
   }
 
   tick() {
+    if (this.onTick) this.onTick(this.startingValue);
     this.timeRemaining = this.timeRemaining - 0.02;
-    if (this.timeRemaining <= 0) this.pause();
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      this.onComplete();
+    }
+  }
+
+  change() {
+    if (this.onChange) this.onChange();
+    this.pause();
+    this.startingValue = this.durationInput.value;
+    this.isChange = true;
   }
 }
 
